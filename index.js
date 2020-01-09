d8e8d8b54df06aead2a7e81815dbcd7ac0dde297
Stavros Charitakis <sce9sc@hotmail.com>
2020-01-09 13:06:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-try {
-  if (process.env.SODIUM_NATIVE === 'disable') throw new Error('Use sodium-javascript')
-  module.exports = require('sodium-native')
-} catch (err) {
-  module.exports = require('sodium-javascript')
-}
+module.exports = require('sodium-javascript')
+
